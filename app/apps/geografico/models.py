0a918ec9079bf82556221a9608a488568da288c1
LeonardoHQ <you@example.com>
2021-04-30 01:00:15 -0300

    commit_prueba

--- app/apps/geografico/models.py
+++ app/apps/geografico/models.py
@@ -7,5 +7,5 @@ class paises(models.Model):
 
 """ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
+    choice_text = models.CharField(max_length=hhh200)
     votes = models.IntegerField(default=0) """
